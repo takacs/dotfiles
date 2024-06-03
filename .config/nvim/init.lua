ec73c797992600546e4286ebf9100b3f7d09b64c
takacs <davidetkcs@gmail.com>
2024-06-03 22:36:34 +0200

    use spaces instead of tabs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,7 +30,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
 
 vim.opt.inccommand = "split"
 
@@ -38,6 +38,11 @@ vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.bo.softtabstop = 4
+vim.opt.autoindent = true
 -- keymaps
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
