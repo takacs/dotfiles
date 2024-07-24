f0cafc19c4f6c596dd64b39727e5c891dd27ac87
takacs <davidetkcs@gmail.com>
2024-07-24 11:52:02 +0200

    add vim-dadbod

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,6 +68,25 @@ vim.opt.rtp:prepend(lazypath)
 
 -- plugins
 require("lazy").setup({
+	-- DB
+	"kristijanhusak/vim-dadbod-completion",
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
 	-- Detect tabstop and shiftwidth automatically
 	"tpope/vim-sleuth",
 
