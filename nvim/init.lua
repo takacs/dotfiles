c5cdecc8c89c6263090b87b7fc0c6de560b550d7
David Takacs <davidetkcs@gmail.com>
2025-09-22 23:42:38 +0200

    add lsp loading from lsp folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,42 @@
-require("config.lazy")
+vim.loader.enable()
 
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
 -- Highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
+vim.lsp.enable({
+  -- lua
+  "lua_ls",
+  -- nix
+  --
+  --[[
+  "nil_ls",
+  "nixd",
+  -- python
+  "pyright",
+  "ruff",
+  -- markdown
+  "ltex",
+  -- terraform
+  "terraformls",
+  -- yaml
+  "yamlls",
+  -- bash
+  "bashls"
+  ]] --
+})
 
-
+require("config.lazy")
